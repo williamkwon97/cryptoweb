df34f83852c2be8f70e5c184fd5a7f704c7d65a5
Transadmin <transadmin@williamkwon.local>
2021-02-24 07:50:02 -0600

    first commit

---
+++ react-api-crpyto-tracker/src/App.js
@@ -0,0 +1,42 @@
+import React ,{useState, useEffect} from 'react';
+import axios from 'axios'
+import './App.css';
+
+
+
+function App() {
+
+  const [coins, setCoins] = useState([]);
+
+
+  useEffect(() => {
+    axios
+      .get(
+        'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=false'
+      )
+      .then(res => {
+        setCoins(res.data);
+        console.log(res.data);
+      })
+      .catch(error => console.log(error));
+  }, []);
+
+
+  return (
+    <div className='coin-app'>
+      <div className='coin-search'>
+        <h1 className='coin-text'>Search a currency</h1>
+        <form>
+          <input 
+            type='text'
+            placeholder = 'search a currency'
+            className = 'coin-input'/>
+          
+        </form>
+      </div>
+      </div>
+     
+  );
+}
+
+export default App;
